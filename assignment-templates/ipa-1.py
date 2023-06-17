52bc8a5dc0256fe257d76e66c5fd01195320f126
Alaina Ligot <alaina@Alainas-MacBook-Air.local>
2023-06-17 12:56:25 +0800

    Assignment 1

---
+++ assignment-templates/ipa-1.py
@@ -0,0 +1,25 @@
+
+def savings(gross_pay, tax_rate, expenses):
+    # Deduct tax rate
+    gross_pay = (gross_pay * (1 - tax_rate)) // 1
+    # Subtract expenses
+    gross_pay -= expenses
+    return int(gross_pay)
+
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    material_consumed = num_jobs * job_consumption
+    waste = total_material - material_consumed
+    return f"{waste} {material_units}"
+
+
+def interest(principal, rate, periods):
+    simple_interest = principal * rate * periods
+    return principal + simple_interest
+
+
+def body_mass_index(weight, height):
+    kg = weight * 0.453592
+    m = ((height[0] * 12) + height[1]) * 0.0254
+    BMI = kg / (m * m)
+    return BMI
