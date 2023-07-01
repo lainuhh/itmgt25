f1a38627b9e57f2bb04f1293f7e11f11c3e53203
Alaina Ligot <alaina@Alainas-MacBook-Air.local>
2023-07-01 19:42:51 +0800

    Assignment 2 Re-submission (again)

--- assignment-templates/ipa-2.py
+++ assignment-templates/ipa-2.py
@@ -1,13 +1,13 @@
 
 def shift_letter(letter, shift):
-    new_position = (ord(letter.capitalize()) + shift) % 26
+    new_position = ((ord(letter) + (shift + 65)) % 26) + 65
     return chr(new_position)
 
 
 def caesar_cipher(message, shift):
     encrypted_msg = ''
     for character in message:
-        new_position = (ord(character.capitalize()) + shift) % 26
+        new_position = ((ord(character) + (shift + 65)) % 26) + 65
         encrypted_msg += chr(new_position)
     return encrypted_msg
 
@@ -16,20 +16,26 @@ def shift_by_letter(letter, letter_shift):
     if letter == " ":
         shifted_letter = " "
     else:
-        shift_num = ord(letter_shift.capitalize())
-        new_position = (ord(letter.capitalize()) + shift_num) % 26
+        shift_num = ord(letter_shift)
+        new_position = ((ord(letter) + shift_num) % 26) + 65
         shifted_letter = chr(new_position)
     return shifted_letter
 
 
 def vigenere_cipher(message, key):
     encrypted_msg = ''
+    extended_key = ''
+
     for i in range(len(message)):
+        extended_key += key[(i % len(key))]
+
+    for i in range(len(message)):
+        print(f"Editing char: {message[i]} | {ord(message[i])}")
         if message[i] == " ":
             encrypted_msg += " "
         else:
             key_index = i % len(key)
-            new_position = (ord(message[i].capitalize()) + ord(key[key_index])) % 26
+            new_position = ((ord(message[i]) + ord(extended_key[key_index])) % 26) + 65
             encrypted_msg += chr(new_position)
     return encrypted_msg
 
@@ -50,3 +56,4 @@ def scytale_decipher(message, shift):
     for i in range(shift):
         decoded_message += message[i::shift]
     return decoded_message
+
